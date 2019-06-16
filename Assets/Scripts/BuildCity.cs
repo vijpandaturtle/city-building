9a144ec281fcf60038121395c7e58c86d7092de2
Vijayasri Iyer <thisisvij98@gmail.com>
2019-06-17 00:27:58 +0530

    Initial Commit
    Spawned Buildings

---
+++ Assets/Scripts/BuildCity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildCity : MonoBehaviour
+{
+    public GameObject[] buildings;
+    // Smaller map size for testing 
+    public int mapWidth = 20;
+    public int mapHeight = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int h = 0; h < mapHeight; h++)
+        {
+            for(int w = 0; w < mapWidth; w++)
+            {
+                Vector3 pos = new Vector3(w, 0, h);
+                int n = Random.Range(0, buildings.Length);
+                Instantiate(buildings[n], pos, Quaternion.identity);
+            }
+        }
+    }
+
+}
