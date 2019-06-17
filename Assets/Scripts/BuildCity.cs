2602dec732f5cd7d0b1a63f0ec4c5d8b4ded8fa5
Vijayasri Iyer <thisisvij98@gmail.com>
2019-06-17 16:56:13 +0530

    Spawned buildings using perlin noise

--- Assets/Scripts/BuildCity.cs
+++ Assets/Scripts/BuildCity.cs
@@ -8,6 +8,7 @@ public class BuildCity : MonoBehaviour
     // Smaller map size for testing 
     public int mapWidth = 20;
     public int mapHeight = 20;
+    int buildingFootprint = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,19 @@ public class BuildCity : MonoBehaviour
         {
             for(int w = 0; w < mapWidth; w++)
             {
-                Vector3 pos = new Vector3(w, 0, h);
-                int n = Random.Range(0, buildings.Length);
-                Instantiate(buildings[n], pos, Quaternion.identity);
+                float seed = 55; // Random.Range(0, 100);
+                int result = (int)(Mathf.PerlinNoise(w/10.0f + seed, h/10.0f + seed) * 10);
+                Vector3 pos = new Vector3(w * buildingFootprint, 0, h * buildingFootprint);
+                if (result < 2)
+                    Instantiate(buildings[0], pos, Quaternion.identity);
+                else if (result < 4)
+                    Instantiate(buildings[1], pos, Quaternion.identity);
+                else if (result < 6)
+                    Instantiate(buildings[2], pos, Quaternion.identity);
+                else if (result < 8)
+                    Instantiate(buildings[3], pos, Quaternion.identity);
+                else if (result < 10)
+                    Instantiate(buildings[4], pos, Quaternion.identity);
             }
         }
     }
